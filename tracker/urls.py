56b8e573d5114428a284ac452f2f664e0053fc13
Ankush Singh <ankushsingh@mac.mynetworksettings.com>
2025-08-26 12:49:16 -0400

    Add edit/delete views, routes, and templates for JobApplication

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, application_list
+from .views import home, application_list, application_create, application_edit, application_delete
 
 urlpatterns = [
     path("", home, name="home"),
     path("applications/", application_list, name="application_list"),
+    path("applications/new/", application_create, name="application_create"),
+    path("applications/<int:pk>/edit/", application_edit, name="application_edit"),
+    path("applications/<int:pk>/delete/", application_delete, name="application_delete"),
 ]
-
 
--- tracker/views.py
+++ tracker/views.py
@@ -9,3 +9,38 @@ def application_list(request):
     applications = JobApplication.objects.all().order_by("-date_applied")
     return render(request, "tracker/application_list.html", {"applications": applications})
 
+from django.shortcuts import render, redirect
+from .forms import JobApplicationForm
+
+def application_create(request):
+    if request.method == "POST":
+        form = JobApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("application_list")
+    else:
+        form = JobApplicationForm()
+    return render(request, "tracker/application_form.html", {"form": form})
+
+from django.shortcuts import get_object_or_404, redirect
+from .forms import JobApplicationForm
+from .models import JobApplication
+
+def application_edit(request, pk):
+    app = get_object_or_404(JobApplication, pk=pk)
+    if request.method == "POST":
+        form = JobApplicationForm(request.POST, instance=app)
+        if form.is_valid():
+            form.save()
+            return redirect("application_list")
+    else:
+        form = JobApplicationForm(instance=app)
+    return render(request, "tracker/application_form.html", {"form": form, "editing": True, "app": app})
+
+def application_delete(request, pk):
+    app = get_object_or_404(JobApplication, pk=pk)
+    if request.method == "POST":
+        app.delete()
+        return redirect("application_list")
+    return render(request, "tracker/application_confirm_delete.html", {"app": app})
+
